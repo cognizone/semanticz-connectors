8fe21ffd73ca0e607714962b817489e258315869
psiotwo <petr.kremen@cogni.zone>
2024-09-24 20:17:19 +0200

    chore: removing unused code

--- semanticz-connectors-common/src/main/java/zone/cogni/semanticz/connectors/CognizoneException.java
+++ semanticz-connectors-common/src/main/java/zone/cogni/semanticz/connectors/CognizoneException.java
@@ -4,16 +4,6 @@ public class CognizoneException extends RuntimeException {
 
   private static final long serialVersionUID = 1L;
 
-  public static CognizoneException rethrow(Throwable exception) {
-    if (exception instanceof Error) {
-      throw (Error) exception;
-    }
-    if (exception instanceof RuntimeException) {
-      throw (RuntimeException) exception;
-    }
-    throw new CognizoneException(exception);
-  }
-
   public CognizoneException(Throwable cause) {
     super(cause);
   }
