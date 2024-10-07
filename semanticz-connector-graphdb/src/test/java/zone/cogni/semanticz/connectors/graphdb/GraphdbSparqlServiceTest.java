4382f5fa4e6b243cf1a82ad20d3c89b76d6e9cab
psiotwo <petr.kremen@cogni.zone>
2024-10-07 11:03:30 +0200

    fix: GraphDb test

--- semanticz-connector-graphdb/src/test/java/zone/cogni/semanticz/connectors/graphdb/GraphdbSparqlServiceTest.java
+++ semanticz-connector-graphdb/src/test/java/zone/cogni/semanticz/connectors/graphdb/GraphdbSparqlServiceTest.java
@@ -12,18 +12,18 @@ import java.util.function.Function;
 @Disabled("An integration test dependent on a running GraphDB instance. To run it manually, set the GraphDBConfig below properly and run the tests.")
 public class GraphdbSparqlServiceTest extends AbstractSparqlServiceTest<GraphDBSparqlService> {
 
-  @Override
-  protected void disposeSUT(GraphDBSparqlService sparqlService) {
-    // do nothing
-  }
-
   public GraphDBSparqlService createSUT() {
     final GraphDBConfig config = new GraphDBConfig();
-    config.setUrl("http://localhost:7200/");
+    config.setUrl("http://localhost:7200");
     config.setRepository("test");
     config.setUser("test");
     config.setPassword("test");
     return new GraphDBSparqlService(config);
+  }
+
+  @Override
+  protected void disposeSUT(GraphDBSparqlService sparqlService) {
+    // do nothing
   }
 
   @Test
