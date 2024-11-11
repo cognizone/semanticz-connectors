245b1a6454006037c7d791cb2e700dda3ebfcaf3
salihkadir <139787733+salihkadir@users.noreply.github.com>
2024-11-11 15:17:30 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.id == "pl.allegro.tech.build.axion-release") {
-                useVersion("1.13.3")
+                useVersion("1.14.0")
             }
         }
     }
