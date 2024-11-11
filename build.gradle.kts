342739b77167634678a046e3c2d87a4b231750d7
salihkadir <139787733+salihkadir@users.noreply.github.com>
2024-11-11 09:53:31 +0200

    Update build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,92 @@
+plugins {
+    `java-library`
+    pmd
+    jacoco
+    id("io.freefair.lombok") version "8.10"
+    id("org.owasp.dependencycheck") version "10.0.3"
+    id("maven-publish")
+    id("signing")
+    id("pl.allegro.tech.build.axion-release") version "1.13.3"
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+
+group = "zone.cogni.semanticz"
+
+
+version = scmVersion.version
+scmVersion {
+    tag.apply {
+        prefix = "v"
+        versionSeparator = ""
+        branchPrefix = mapOf(
+            "release/.*" to "release-v",
+            "hotfix/.*" to "hotfix-v"
+        )
+    }
+    nextVersion.apply {
+        suffix = "SNAPSHOT"
+        separator = "-"
+    }
+    versionIncrementer("incrementPatch") // Increment the patch version
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name.set("semanticz-connectors")
+                description.set("This repository contains connectors to RDF stores. Its goal is to provide unified API for accessing various back-end implementations and to optimize them in future with implementations more optimized towards individual stores.")
+                url.set("https://github.com/cognizone/semanticz-connectors")
+
+                scm {
+                    connection.set("scm:git@github.com:cognizone/semanticz-connectors.git")
+                    developerConnection.set("scm:git@github.com:cognizone/semanticz-connectors.git")
+                    url.set("https://github.com/cognizone/semanticz-connectors")
+                }
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("cognizone")
+                        name.set("Cognizone")
+                        email.set("semanticz@cogni.zone")
+                    }
+                }
+            }
+        }
+    }
+
+    repositories {
+        if (project.hasProperty("publishToMavenCentral")) {
+            maven {
+                credentials {
+                    username = System.getProperty("ossrh.username")
+                    password = System.getProperty("ossrh.password")
+                }
+                val stagingRepoUrl = "${System.getProperty("ossrh.url")}/service/local/staging/deploy/maven2"
+                val snapshotsRepoUrl = "${System.getProperty("ossrh.url")}/content/repositories/snapshots"
+                url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else stagingRepoUrl)
+            }
+        }
+    }
+}
+
+
+signing {
+    if (project.hasProperty("publishToMavenCentral")) {
+        sign(publishing.publications["mavenJava"])
+    }
+}
