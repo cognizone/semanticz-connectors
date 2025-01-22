985fd28593ace031cbb6a0462c052d0786442c67
psiotwo <petr.kremen@cogni.zone>
2025-01-22 10:53:09 +0100

    chore: minor cleanup

--- semanticz-connectors-common/src/main/java/zone/cogni/semanticz/connectors/utils/Constants.java
+++ semanticz-connectors-common/src/main/java/zone/cogni/semanticz/connectors/utils/Constants.java
@@ -23,8 +23,8 @@ public final class Constants {
   public static final String APPLICATION_FORM_URLENCODED_VALUE = "application/x-www-form-urlencoded";
   public static final String APPLICATION_SPARQL_RESULTS_XML = "application/sparql-results+xml";
   public static final String APPLICATION_SPARQL_QUERY = "application/sparql-query";
-  public static final String CONTENT_TYPE = "Content-Type";
   public static final String TEXT_TURTLE = "text/turtle";
+  public static final String CONTENT_TYPE = "Content-Type";
 
   private Constants() {
     throw new UnsupportedOperationException();
